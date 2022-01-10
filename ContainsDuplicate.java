e02ba7a540320841f6932f3d2b9461796e13ccb9
Rojina Amatya <rojinaamatya1432@gmail.com>
2022-01-10 13:05:57 -0600

    Solved #217

---
+++ ContainsDuplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+
+        for(int i:nums){
+            if (!set.add(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
